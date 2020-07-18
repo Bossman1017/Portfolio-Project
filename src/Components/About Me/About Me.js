7f4d10d9138b22b34dadb40110ac1d72707f698e
Naven Kennedy <r2h@AdminisatorsMBP.attlocal.net>
2020-07-17 20:21:59 -0400

    changesadded

--- src/Components/About Me/About Me.js
+++ src/Components/About Me/About Me.js
@@ -5,28 +5,30 @@ import './About Me.css';
 const AboutMe = () => {
     return (
         <div>
-            <h1 className = "About_header">Who Am I ...</h1>
-           
-            <div className = "About_container">
-                <div className = "star-five">
-                
+            <h1 className="About_header">Who Am I ...</h1>
+
+            <div className="About_container">
+                <div className="star-five">
+
+                </div>
+                <div className="about-box">
+                    <div className="about-text">
+                        <p className="text_1">Hello my name is Naven Kennedy ,I'm an aspiring and dedicated front-end developer who is a soon-to-be graduate of the Road To Hire Coding Fellowship. Road To Hire Program is a strenuous 6 month program that aims to develop successful students in the software development field.Throughout this fellowship I have gained many improvements in my programming skills. These skills include working with Git, Github and the terminal. Languages that include HTML, CSS, JavaScript, Node, and React. I've also gained many professional development skills that include time management, email etiquette, debating, team building and problem solving.  </p>
             </div>
-            <div className="about-box">
-            <div className = "about-text">
-            <p className = "text_1">Hello my name is Naven Kennedy ,I'm an aspiring and dedicated front-end developer who is a soon-to-be graduate of the Road To Hire Coding Fellowship. Road To Hire Program is a strenuous 6 month program that aims to develop successful students in the software development field.Throughout this fellowship I have gained many improvements in my programming skills. These skills include working with Git, Github and the terminal. Languages that include HTML, CSS, JavaScript, Node, and React. I've also gained many professional development skills that include time management, email etiquette, debating, team building and problem solving.  </p>
-            </div>
-            <div className ="about_imgbox">
-            <img className = "About_picture" src = "img/Kennedy.jpg" alt="hello"></img>
-            </div>
-            </div>
-            </div>
-            <img className = "html_footer2" src = "img/html.jpg"></img>  
-            <img className = "css_footer2" src = "img/css.jpg"></img>  
-            <img className = "js_footer2" src = "img/js.png"></img>  
-            <img className = "react_footer2" src = "img/react1.png"></img>  
-            <img className = "mysql_footer" src = "img/mysql.svg"></img>  
-           </div>
-            )
+                    <div className ="about_imgbox">
+                    <img className= "About_picture" src = "img/Kennedy.jpg" alt="hello"></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="skills-div">
+                    <img className="html_footer2" src="img/html.jpg"></img>
+                    <img className="css_footer2" src="img/css.jpg"></img>
+                    <img className="js_footer2" src="img/js.png"></img>
+                    <img className="react_footer2" src="img/react1.png"></img>
+                    <img className="mysql_footer" src="img/mysql.svg"></img>
+                    </div>
+                </div>
+             )
         }
 
 export default AboutMe;
